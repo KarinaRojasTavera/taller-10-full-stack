aedabcd89cdacc71e7b211d203d326c61586d2ab
KarinaRojasTavera <karinatavera@gmail.com>
2023-06-23 07:19:59 -0500

    adicion punto 4 por

--- punto-4/controller/calculationController.php
+++ punto-4/controller/calculationController.php
@@ -1,1 +1,30 @@
+<?php  
+$resulado= "";
+$fechaNacimiento = 0;
+$fechaActual = 0;
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    // Obtener la fecha de nacimiento dada por el usuario
+    $fechaNacimiento = $_POST['fecha_nacimiento'];
+
+    // Obtener la fecha actual
+    $fechaActual = date('Y-m-d');
+
+    // Calcular la edad
+
+    $diff = date_diff(date_create($fechaNacimiento), date_create($fechaActual));
+    $edad = $diff->y;
+
+    // Realizar la comparación y mostrar el resultado
+
+    if ($edad <18) {
+      $resulado =  "Su edad es " . $edad . ", por tanto: No es mayor de edad";
+    } else {
+      $resulado = "Su edad es " . $edad . ", por tanto: Es mayor de edad";
+    }
+}
+?>
+
+
 
---
+++ punto-4/view/components/result.php
@@ -0,0 +1,4 @@
+<div>
+  <p>Resultado:</p>
+  <h3><?php echo $resulado; ?></h3>  
+</div>
